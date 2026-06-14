56f08f1190eb30ecf010c035f939ab682f9c9e22
Rashid <ra1315921@gmail.com>
2026-06-14 13:15:33 +0000

    added payment API

---
+++ payment.py
@@ -0,0 +1,8 @@
+from flask import Flask
+app=Flask(__name__)
+@app.route('/')
+def Hello_world():
+    return "hello flask"
+if '__name__'=='__main__':
+    app.run(debug=True, host='127.0.0.1', port=5000)
+
